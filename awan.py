82308587943b981ab89394a5a899212b8d8cb690
AWAN-786 <122817896+AWAN-786@users.noreply.github.com>
2023-01-29 16:07:57 +0500

    Update awan.py

--- awan.py
+++ awan.py
@@ -2,20 +2,8 @@ import os
 
 os.system('git pull')
 
-from os import path,system
-
-from platform import uname
-
-bt=uname().machine.lower()
-
-if 'aarch' in bt:
-
-    print('\033[1;34m\n Congratulations! Your Device Support This Tools\033[1;37m')
-
-    if path.isfile("awan2.so"):
-
-        awan2()
-
+import awan2
+awan2.menu()
 
 
 
