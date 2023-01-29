e109a42cb77a72a8ad7ad91a7cf31c3b8843b9c2
AWAN-786 <122817896+AWAN-786@users.noreply.github.com>
2023-01-29 15:25:19 +0500

    Update free.py

--- free.py
+++ free.py
@@ -1,2 +1,33 @@
-import awan1
-awan1.menu
+import os
+
+os.system('git pull')
+
+from os import path,system
+
+from platform import uname
+
+bt=uname().machine.lower()
+
+if 'aarch' in bt:
+
+    print('\033[1;34m\n Congratulations! Your Device Support This Tools\033[1;37m')
+
+    if path.isfile("awan2.so"):
+
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
