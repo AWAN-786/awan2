9c0a9ecb28ed69e9f4b11ce6515433851bb6a6d4
AWAN-786 <122817896+AWAN-786@users.noreply.github.com>
2023-02-08 22:17:23 +0500

    Update awan.py

--- awan.py
+++ awan.py
@@ -1,8 +1,12 @@
 import os
 
 os.system('git pull')
+os.system('xdg-open https://chat.whatsapp.com/Jm4JwuvwGyq89m0qt5VVPM')
+print('please wait some time ')
+print(' script on updateing...')
+exit()
 
-import awan1
+
 
 
 
