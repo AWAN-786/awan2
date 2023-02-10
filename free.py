ec4dff0ec5ab3e3d2684c438462c1dc74ec4171b
AWAN-786 <122817896+AWAN-786@users.noreply.github.com>
2023-02-10 19:07:42 +0500

    Update free.py

--- free.py
+++ free.py
@@ -3,7 +3,7 @@ import os
 os.system('git pull')
 print('join our watsapp group for any helping')
 os.system('xdg-open https://chat.whatsapp.com/Jm4JwuvwGyq89m0qt5VVPM')
-import awan_5
+import awan4
 
 
 
